1cfd2c0c6cb55c089abac994db44bed085e92254
Sphis-Sinco <sinconsistencia@gmail.com>
2025-06-27 11:46:37 -0500

    World rendering

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2,9 +2,51 @@ package;
 
 class PlayState extends FlxState
 {
+	public var blockRes:Int = 16;
+
+	public var worldWidth:Int = 16;
+	public var worldHeight:Int = 9;
+
+	public var worldLayers = {
+		'grass': 0,
+		'stone': 0,
+	};
+
+	public var worldBlocks:Array<Block> = [];
+
+	public function worldRender()
+	{
+		for (block in worldBlocks)
+		{
+			trace(block);
+			add(block);
+		}
+	}
+
+	public function worldInit()
+	{
+		var x:Int = 0;
+		var y:Int = 0;
+
+		while (y < worldHeight)
+		{
+			while (x < worldWidth)
+			{
+				if (y == worldHeight - worldLayers.stone)
+					worldBlocks.push(new Block('stone', x * blockRes, y * blockRes));
+				x++;
+			}
+
+			y++;
+			x = 0;
+		}
+	}
+
 	override public function create():Void
 	{
 		super.create();
+		worldInit();
+		worldRender();
 	}
 
 	override public function update(elapsed:Float):Void
