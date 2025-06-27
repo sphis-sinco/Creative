10308fa8ab841fd92b0e1a09f6d063fa93954683
Sphis-Sinco <sinconsistencia@gmail.com>
2025-06-27 18:25:28 -0500

    All buttons have the same width

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -12,6 +12,7 @@ class MenuState extends FlxState
 		var VersionText:FlxText = new FlxText(10, 10, 0, 'Creative ' + Version.generateVersionString(true, true, true), 16);
 		add(VersionText);
 		VersionText.scrollFactor.set(0, 0);
+		VersionText.text += '\nMade by sphis_sinco';
 
 		var logo:FlxSprite = new FlxSprite(0, 0).loadGraphic(FileManager.getImageFile('logo/logo'));
                 add(logo);
@@ -19,10 +20,12 @@ class MenuState extends FlxState
 		logo.screenCenter(X);
 		logo.y -= (logo.height / 3);
 
+		var btnWidth:Int = 125;
+
 		var playRegular:FlxButtonPlus = new FlxButtonPlus(0, 0, () ->
 		{
 			FlxG.switchState(() -> new PlayState());
-		}, 'Play (Regular world)', 125);
+		}, 'Play (Regular world)', btnWidth);
 		add(playRegular);
 		playRegular.screenCenter();
 		playRegular.scale.set(2, 2);
@@ -30,7 +33,7 @@ class MenuState extends FlxState
 		var playBlank:FlxButtonPlus = new FlxButtonPlus(0, 0, () ->
 		{
 			FlxG.switchState(() -> new PlayState('assets/templates/blank'));
-		}, 'Play (Blank world)');
+		}, 'Play (Blank world)', btnWidth);
 		add(playBlank);
 		playBlank.screenCenter();
 		playBlank.scale.set(2, 2);
