e9013a7030b2d848c556986995fa482747ecb0a4
Sphis-Sinco <sinconsistencia@gmail.com>
2025-06-28 08:26:48 -0500

    Pack file

--- source/import.hx
+++ source/import.hx
@@ -2,12 +2,15 @@ import flixel.*;
 import flixel.graphics.frames.FlxAtlasFrames;
 import haxe.*;
 import lime.utils.Assets;
+import resourcepacks.*;
+import resourcepacks.Pack.Pack;
+import resourcepacks.Pack.PackClass;
 import sinlib.*;
 import sinlib.utilities.*;
 
 using StringTools;
-
 #if sys
 import sys.FileSystem;
 import sys.io.File;
 #end
+
---
+++ source/resourcepacks/Pack.hx
@@ -0,0 +1,10 @@
+package resourcepacks;
+
+class PackClass {
+        public static var PACK_FORMAT:Int = 1;
+}
+
+typedef Pack = {
+        var name:String;
+        var format:Int;
+}
