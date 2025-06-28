034daed635acd4f78b4f79b9906c3acf81e00c94
Sphis-Sinco <sinconsistencia@gmail.com>
2025-06-28 07:17:29 -0500

    version bump

--- source/Version.hx
+++ source/Version.hx
@@ -3,8 +3,8 @@ package;
 class Version {
         public static var MajorVersion:Int = 0;
 	public static var MinorVersion:Int = 3;
-	public static var PatchVersion:Int = 3;
-	public static var HotfixVersion:Int = 2;
+	public static var PatchVersion:Int = 4;
+	public static var HotfixVersion:Int = 0;
         public static var Suffix:String = '';
 
         public static function generateVersionString(patch:Bool, hotfix:Bool, suffix:Bool)
